be3d228709d4032fd1dbe63b4f9ee50fc3958b4a
XC [ XYAA CODE ] <109187416+Xyaa-Code@users.noreply.github.com>
2022-09-16 15:22:45 +0700

    Add files via upload

---
+++ login.py
@@ -0,0 +1,21 @@
+# coding:utf-8
+#/usr/bin/python
+import os,requests,time
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+
+try:
+    xyaa_reg = requests.get("http://ip-api.com/json/").json()
+    xyaa_xcc = xyaa_reg["country"]
+    
+except:
+        xyaa_xcc = "Indonesia"
+        
+if __name__ == "__main__":
+	if "Indonesia" == xyaa_xcc:
+		os.system('git pull')
+		__import__("XMBF").pil_sen()
+	else:
+	     os.system("clear")
+	     print(f"{P}[{M}!{P}] sorry, this script can only be used in Indonesia")
