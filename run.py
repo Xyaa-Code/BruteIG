2506954ab34b29ffc8937be675252df16870adaf
Aditya Putra Mahesa XD <109187416+Xyaa-Code@users.noreply.github.com>
2022-10-26 21:53:20 +0700

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("run_enc").cek_lisensi_aktif()
+       __import__("run").cek_lisensi_aktif()
    except Exception as e:
        exit(str(e))
